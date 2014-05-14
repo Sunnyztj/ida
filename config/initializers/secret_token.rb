3dd98263137cea70dd2ef923d87f4902259036fc
sunny <webztj@gmail.com>
2014-05-14 20:54:57 +0930

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ida::Application.config.secret_token = '455d2d68c8a31cdc098c538f5035fd26a70226a18d52c16aa9b3a2843a12e6efc878945f0748642d4f5831f8e43adcfa0f84892886b8e8de4fbadb091fb398d7'
